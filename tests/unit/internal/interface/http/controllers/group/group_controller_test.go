566116dbf34a48a136400e61ec56e2245788d04d
Andreis3 <andrei.as3@hotmail.com>
2024-04-02 10:11:05 -0300

    refactor: adjust group_controller_test

--- tests/unit/internal/interface/http/controllers/group/group_controller_test.go
+++ tests/unit/internal/interface/http/controllers/group/group_controller_test.go
@@ -40,7 +40,61 @@ var _ = Describe("INTERFACE :: HTTP :: CONTROLLERS :: GROUP :: GROUP_CONTROLLER"
 		var requestIDMock *helpers_mock.RequestIDMock
 		var groupController *group_controller.Controller
 		Context("When I call the method CreateGroup", func() {
-			BeforeEach(func() {
+			It("Should return a error when of method insertGroupCommand.Execute is call", func() {
+				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
+					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
+						return group_dto.GroupOutputDTO{}, &util.ValidationError{
+							Code:        "VBR-400",
+							Status:      http.StatusBadRequest,
+							ClientError: []string{"error test"},
+							LogError:    []string{"error test"},
+						}
+					},
+				}
+				requestIDMock = &helpers_mock.RequestIDMock{
+					GenerateFunc: func() string {
+						return "123"
+					},
+				}
+				loggerMock = &logger_mock.LoggerMock{
+					ErrorFunc: func(message string, keysAndValues ...any) {
+						return
+					},
+				}
+
+				groupController = group_controller.NewGroupController(groupCommandMock, loggerMock, requestIDMock)
+				body := `{
+							"group_name":"teste 1",
+							"code": "23",
+							"status":"active"
+						  }`
+				request, err := http.NewRequest("POST", "/group", strings.NewReader(body))
+				writer := httptest.NewRecorder()
+
+				expected := helpers.TypeResponseError{
+					RequestID:    "123",
+					CodeError:    "VBR-400",
+					StatusCode:   http.StatusBadRequest,
+					ErrorMessage: []interface{}{"error test"},
+				}
+
+				result := helpers.TypeResponseError{}
+
+				groupController.CreateGroup(writer, request)
+
+				json.Unmarshal(writer.Body.Bytes(), &result)
+
+				Expect(err).To(BeNil())
+				Expect(request).NotTo(BeNil())
+				Expect(writer.Code).To(Equal(http.StatusBadRequest))
+				Expect(result).To(Equal(expected))
+				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
+				Expect(result.StatusCode).To(Equal(expected.StatusCode))
+				Expect(loggerMock.ErrorMSG).To(Equal("Create Group Error"))
+				Expect(loggerMock.ErrorAny).To(Equal([]interface{}{"REQUEST_ID", "123", "CODE_ERROR", "VBR-400", "ERROR_MESSAGE", "error test"}))
+			})
+
+			It("Should create a new group without errors", func() {
 				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
 					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
 						return group_dto.GroupOutputDTO{
@@ -59,14 +113,7 @@ var _ = Describe("INTERFACE :: HTTP :: CONTROLLERS :: GROUP :: GROUP_CONTROLLER"
 					},
 				}
 				loggerMock = &logger_mock.LoggerMock{}
-			})
-			AfterEach(func() {
-				groupCommandMock = nil
-				loggerMock = nil
-				requestIDMock = nil
-				groupController = nil
-			})
-			It("Should create a new group", func() {
+
 				groupController = group_controller.NewGroupController(groupCommandMock, loggerMock, requestIDMock)
 				body := `{
     						"group_name":"teste 1",
@@ -101,71 +148,6 @@ var _ = Describe("INTERFACE :: HTTP :: CONTROLLERS :: GROUP :: GROUP_CONTROLLER"
 				Expect(result).To(Equal(expected))
 				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
 				Expect(result.StatusCode).To(Equal(expected.StatusCode))
-
-			})
-		})
-
-		Context("When I call the method Execute return error", func() {
-			BeforeEach(func() {
-				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
-					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
-						return group_dto.GroupOutputDTO{}, &util.ValidationError{
-							Code:        "VBR-400",
-							Status:      http.StatusBadRequest,
-							ClientError: []string{"error test"},
-							LogError:    []string{"error test"},
-						}
-					},
-				}
-				requestIDMock = &helpers_mock.RequestIDMock{
-					GenerateFunc: func() string {
-						return "123"
-					},
-				}
-				loggerMock = &logger_mock.LoggerMock{
-					ErrorFunc: func(message string, keysAndValues ...any) {
-						return
-					},
-				}
-			})
-			AfterEach(func() {
-				groupCommandMock = nil
-				loggerMock = nil
-				requestIDMock = nil
-				groupController = nil
-			})
-			It("Should return a bad request error", func() {
-				groupController = group_controller.NewGroupController(groupCommandMock, loggerMock, requestIDMock)
-				body := `{
-							"group_name":"teste 1",
-							"code": "23",
-							"status":"active"
-						  }`
-				request, err := http.NewRequest("POST", "/group", strings.NewReader(body))
-				writer := httptest.NewRecorder()
-
-				expected := helpers.TypeResponseError{
-					RequestID:    "123",
-					CodeError:    "VBR-400",
-					StatusCode:   http.StatusBadRequest,
-					ErrorMessage: []interface{}{"error test"},
-				}
-
-				result := helpers.TypeResponseError{}
-
-				groupController.CreateGroup(writer, request)
-
-				json.Unmarshal(writer.Body.Bytes(), &result)
-
-				Expect(err).To(BeNil())
-				Expect(request).NotTo(BeNil())
-				Expect(writer.Code).To(Equal(http.StatusBadRequest))
-				Expect(result).To(Equal(expected))
-				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
-				Expect(result.StatusCode).To(Equal(expected.StatusCode))
-				Expect(loggerMock.ErrorMSG).To(Equal("Create Group Error"))
-				Expect(loggerMock.ErrorAny).To(Equal([]interface{}{"REQUEST_ID", "123", "CODE_ERROR", "VBR-400", "ERROR_MESSAGE", "error test"}))
-
 			})
 		})
 	})
