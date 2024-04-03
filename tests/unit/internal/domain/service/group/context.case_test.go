aaef1e21ca985409b17a6dd9938c558f8b49852b
Andreis3 <andrei.as3@hotmail.com>
2024-04-03 14:10:23 -0300

    refactor: adjust test insert_group_service_test

---
+++ tests/unit/internal/domain/service/group/context.case_test.go
@@ -0,0 +1,98 @@
+//go:build unit
+// +build unit
+
+package group_service_test
+
+import (
+	"context"
+	"github.com/andreis3/stores-ms/internal/infra/repository/postgres/group"
+	"github.com/andreis3/stores-ms/internal/infra/uow/interfaces"
+	"github.com/andreis3/stores-ms/internal/util"
+	"github.com/andreis3/stores-ms/tests/mock/infra/repository/postgres/group"
+	"github.com/andreis3/stores-ms/tests/mock/infra/uow"
+	"net/http"
+)
+
+func Success() *uow_mock.UnitOfWorkMock {
+	var unitOfWork *uow_mock.UnitOfWorkMock
+	var mapRegister []uow_mock.RegisterRepository
+	var groupRepositoryMock *repo_group_mock.GroupRepositoryMock
+
+	mapRegister = make([]uow_mock.RegisterRepository, 0)
+	groupRepositoryMock = &repo_group_mock.GroupRepositoryMock{
+		InsertGroupFunc: func(group repo_group.GroupModel) (string, *util.ValidationError) {
+			return "1", nil
+		},
+	}
+	mapRegister = []uow_mock.RegisterRepository{
+		{
+			Key:  util.GROUP_REPOSITORY_KEY,
+			Repo: groupRepositoryMock,
+		},
+	}
+	unitOfWork = &uow_mock.UnitOfWorkMock{
+		DoFunc: func(ctx context.Context, callback func(iuow.IUnitOfWork) *util.ValidationError) *util.ValidationError {
+			return nil
+		},
+		RegisterFunc: func(name string, callback iuow.RepositoryFactory) {
+		},
+		GetRepositoryFunc: func(name string) any {
+			return nil
+		},
+		CommitOrRollbackFunc: func() *util.ValidationError {
+			return nil
+		},
+		RollbackFunc: func() *util.ValidationError {
+			return nil
+		},
+	}
+	uow := uow_mock.NewProxyUnitOfWorkMock(unitOfWork, mapRegister)
+	return uow
+}
+
+func ReturnErrorGroupRepositoryInsertGroup() *uow_mock.UnitOfWorkMock {
+	var unitOfWork *uow_mock.UnitOfWorkMock
+	var mapRegister []uow_mock.RegisterRepository
+	var groupRepositoryMock *repo_group_mock.GroupRepositoryMock
+
+	mapRegister = make([]uow_mock.RegisterRepository, 0)
+	groupRepositoryMock = &repo_group_mock.GroupRepositoryMock{
+		InsertGroupFunc: func(group repo_group.GroupModel) (string, *util.ValidationError) {
+			return "", &util.ValidationError{
+				Code:        "PIDB-235",
+				Status:      http.StatusInternalServerError,
+				LogError:    []string{"Insert group error"},
+				ClientError: []string{"Internal Server Error"},
+			}
+		},
+	}
+	mapRegister = []uow_mock.RegisterRepository{
+		{
+			Key:  util.GROUP_REPOSITORY_KEY,
+			Repo: groupRepositoryMock,
+		},
+	}
+	unitOfWork = &uow_mock.UnitOfWorkMock{
+		DoFunc: func(ctx context.Context, callback func(iuow.IUnitOfWork) *util.ValidationError) *util.ValidationError {
+			return &util.ValidationError{
+				Code:        "PIDB-235",
+				Status:      http.StatusInternalServerError,
+				LogError:    []string{"Insert group error"},
+				ClientError: []string{"Internal Server Error"},
+			}
+		},
+		RegisterFunc: func(name string, callback iuow.RepositoryFactory) {
+		},
+		GetRepositoryFunc: func(name string) any {
+			return nil
+		},
+		CommitOrRollbackFunc: func() *util.ValidationError {
+			return nil
+		},
+		RollbackFunc: func() *util.ValidationError {
+			return nil
+		},
+	}
+	uow := uow_mock.NewProxyUnitOfWorkMock(unitOfWork, mapRegister)
+	return uow
+}
--- tests/unit/internal/domain/service/group/insert_group_service_test.go
+++ tests/unit/internal/domain/service/group/insert_group_service_test.go
@@ -1,36 +1,24 @@
 //go:build unit
 // +build unit
 
-package group_service__test
+package group_service_test
 
 import (
-	context_group_service_test "github.com/andreis3/stores-ms/tests/unit/internal/domain/service/group/context"
-	"testing"
+	"github.com/andreis3/stores-ms/internal/util"
 
 	. "github.com/onsi/ginkgo/v2"
 	. "github.com/onsi/gomega"
 
 	"github.com/andreis3/stores-ms/internal/domain/service/group_service"
-	group_dto "github.com/andreis3/stores-ms/internal/interface/http/controller/group/dto"
+	"github.com/andreis3/stores-ms/internal/interface/http/controller/group/dto"
 )
-
-func Test_GroupServiceInsertSuite(t *testing.T) {
-	suiteConfig, reporterConfig := GinkgoConfiguration()
-
-	suiteConfig.SkipStrings = []string{"SKIPPED", "PENDING", "NEVER-RUN", "SKIP"}
-	reporterConfig.FullTrace = true
-	reporterConfig.Succinct = true
-
-	RegisterFailHandler(Fail)
-	RunSpecs(t, "Group Service Insert Test Suite ", suiteConfig, reporterConfig)
-}
 
 var _ = Describe("DOMAIN :: SERVICE :: GROUP_SERVICE :: INSERT_GROUP_SERVICE", func() {
 	Describe("#InsertGroup", func() {
 		Context("When I call the method InsertGroup", func() {
 			It("Should insert a new group not errors", func() {
-				uow := context_group_service_test.ContextGroupServiceSuccess()
-				service := group_service.NewInsertGroupService(uow)
+				groupServiceUowDependency := Success()
+				service := group_service.NewInsertGroupService(groupServiceUowDependency)
 				groupInputDTO := group_dto.GroupInputDTO{
 					GroupName: "Group 1",
 					Code:      "G1",
@@ -47,6 +35,28 @@ var _ = Describe("DOMAIN :: SERVICE :: GROUP_SERVICE :: INSERT_GROUP_SERVICE", f
 				Expect(groupOutputDTO.ID).NotTo(BeEmpty())
 				Expect(groupOutputDTO.CreatedAt).NotTo(BeEmpty())
 				Expect(groupOutputDTO.UpdatedAt).NotTo(BeEmpty())
+			})
+
+			It("Should return an error when the method InsertGroup of the repository is call", func() {
+				groupServiceUowDependency := ReturnErrorGroupRepositoryInsertGroup()
+				service := group_service.NewInsertGroupService(groupServiceUowDependency)
+				groupInputDTO := group_dto.GroupInputDTO{
+					GroupName: "Group 1",
+					Code:      "G1",
+					Status:    "active",
+				}
+
+				groupOutputDTO, err := service.InsertGroup(groupInputDTO)
+				expectedError := &util.ValidationError{
+					Code:        "PIDB-235",
+					Status:      500,
+					ClientError: []string{"Internal Server Error"},
+					LogError:    []string{"Insert group error"},
+				}
+
+				Expect(err).ToNot(BeNil())
+				Expect(groupOutputDTO).To(BeZero())
+				Expect(err).To(Equal(expectedError))
 			})
 		})
 	})
