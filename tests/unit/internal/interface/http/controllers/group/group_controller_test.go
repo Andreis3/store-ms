2ab745c9609c8b0c5d81a3d81f9644db6eadd5be
andreis3 <andrei.as3@hotmail.com>
2024-04-02 22:36:40 -0300

    test: implements more tests group_controller_test

--- tests/unit/internal/interface/http/controllers/group/group_controller_test.go
+++ tests/unit/internal/interface/http/controllers/group/group_controller_test.go
@@ -125,7 +125,7 @@ var _ = Describe("INTERFACE :: HTTP :: CONTROLLERS :: GROUP :: GROUP_CONTROLLER"
 
 				expected := helpers.TypeResponseSuccess{
 					RequestID:  "123",
-					StatusCode: 201,
+					StatusCode: http.StatusCreated,
 					Data: map[string]any{
 						"created_at": "23/09/2021 10:00:00",
 						"updated_at": "23/09/2021 10:00:00",
@@ -148,6 +148,169 @@ var _ = Describe("INTERFACE :: HTTP :: CONTROLLERS :: GROUP :: GROUP_CONTROLLER"
 				Expect(result).To(Equal(expected))
 				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
 				Expect(result.StatusCode).To(Equal(expected.StatusCode))
+				Expect(groupCommandMock.FuncParamsInput).To(Equal([]any{group_dto.GroupInputDTO{
+					GroupName: "teste 1",
+					Code:      "23",
+					Status:    "active",
+				}}))
+				Expect(groupCommandMock.FuncParamsOutput).To(Equal([]any{
+					group_dto.GroupOutputDTO{
+						ID:        "123",
+						GroupName: "test 1",
+						Code:      "23",
+						Status:    "active",
+						CreatedAt: "23/09/2021 10:00:00",
+						UpdatedAt: "23/09/2021 10:00:00",
+					},
+					(*util.ValidationError)(nil),
+				}))
+			})
+
+			It("Should return a error when of method DecoderBodyRequest is call with poorly formatted payload", func() {
+				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
+					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
+						return group_dto.GroupOutputDTO{}, &util.ValidationError{}
+					},
+				}
+				requestIDMock = &helpers_mock.RequestIDMock{
+					GenerateFunc: func() string {
+						return "123"
+					},
+				}
+				loggerMock = &logger_mock.LoggerMock{
+					ErrorFunc: func(message string, keysAndValues ...any) {
+						return
+					},
+				}
+
+				groupController = group_controller.NewGroupController(groupCommandMock, loggerMock, requestIDMock)
+				body := `{
+							"group_name":"teste 1",
+							"code": "23",
+							"status":"active"
+						  `
+				request, err := http.NewRequest("POST", "/group", strings.NewReader(body))
+				writer := httptest.NewRecorder()
+
+				expected := helpers.TypeResponseError{
+					RequestID:    "123",
+					CodeError:    "DJ-402",
+					StatusCode:   http.StatusBadRequest,
+					ErrorMessage: []interface{}{"invalid json"},
+				}
+
+				result := helpers.TypeResponseError{}
+
+				groupController.CreateGroup(writer, request)
+
+				json.Unmarshal(writer.Body.Bytes(), &result)
+
+				Expect(err).To(BeNil())
+				Expect(request).NotTo(BeNil())
+				Expect(writer.Code).To(Equal(http.StatusBadRequest))
+				Expect(result).To(Equal(expected))
+				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
+				Expect(result.StatusCode).To(Equal(expected.StatusCode))
+				Expect(loggerMock.ErrorMSG).To(Equal("Create Group Error"))
+				Expect(loggerMock.ErrorAny).To(Equal([]interface{}{"REQUEST_ID", "123", "CODE_ERROR", "DJ-402", "ERROR_MESSAGE", "unexpected EOF"}))
+			})
+
+			It("Should return a error when of method DecoderBodyRequest is call with invalid json syntax", func() {
+				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
+					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
+						return group_dto.GroupOutputDTO{}, &util.ValidationError{}
+					},
+				}
+				requestIDMock = &helpers_mock.RequestIDMock{
+					GenerateFunc: func() string {
+						return "123"
+					},
+				}
+				loggerMock = &logger_mock.LoggerMock{
+					ErrorFunc: func(message string, keysAndValues ...any) {
+						return
+					},
+				}
+
+				groupController = group_controller.NewGroupController(groupCommandMock, loggerMock, requestIDMock)
+				body := `{
+							"group_name":"teste 1",
+							"code": "23",
+							"status":"active",
+						  }`
+				request, err := http.NewRequest("POST", "/group", strings.NewReader(body))
+				writer := httptest.NewRecorder()
+
+				expected := helpers.TypeResponseError{
+					RequestID:    "123",
+					CodeError:    "DJ-400",
+					StatusCode:   http.StatusBadRequest,
+					ErrorMessage: []interface{}{"invalid json syntax"},
+				}
+
+				result := helpers.TypeResponseError{}
+
+				groupController.CreateGroup(writer, request)
+
+				json.Unmarshal(writer.Body.Bytes(), &result)
+
+				Expect(err).To(BeNil())
+				Expect(request).NotTo(BeNil())
+				Expect(writer.Code).To(Equal(http.StatusBadRequest))
+				Expect(result).To(Equal(expected))
+				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
+				Expect(result.StatusCode).To(Equal(expected.StatusCode))
+				Expect(loggerMock.ErrorMSG).To(Equal("Create Group Error"))
+				Expect(loggerMock.ErrorAny).To(Equal([]interface{}{"REQUEST_ID", "123", "CODE_ERROR", "DJ-400", "ERROR_MESSAGE", "invalid character '}' looking for beginning of object key string"}))
+			})
+
+			It("Should return a error when of method DecoderBodyRequest is call with invalid json field type", func() {
+				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
+					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
+						return group_dto.GroupOutputDTO{}, &util.ValidationError{}
+					},
+				}
+				requestIDMock = &helpers_mock.RequestIDMock{
+					GenerateFunc: func() string {
+						return "123"
+					},
+				}
+				loggerMock = &logger_mock.LoggerMock{
+					ErrorFunc: func(message string, keysAndValues ...any) {
+						return
+					},
+				}
+
+				groupController = group_controller.NewGroupController(groupCommandMock, loggerMock, requestIDMock)
+				body := `{
+							"group_name":"teste 1",
+							"code": 23,
+							"status":"active"
+						  }`
+				request, err := http.NewRequest("POST", "/group", strings.NewReader(body))
+				writer := httptest.NewRecorder()
+
+				expected := helpers.TypeResponseError{
+					RequestID:    "123",
+					CodeError:    "DJ-401",
+					StatusCode:   http.StatusBadRequest,
+					ErrorMessage: []interface{}{"invalid json field type"},
+				}
+
+				result := helpers.TypeResponseError{}
+
+				groupController.CreateGroup(writer, request)
+
+				json.Unmarshal(writer.Body.Bytes(), &result)
+
+				Expect(err).To(BeNil())
+				Expect(request).NotTo(BeNil())
+				Expect(writer.Code).To(Equal(http.StatusBadRequest))
+				Expect(result).To(Equal(expected))
+				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
+				Expect(result.StatusCode).To(Equal(expected.StatusCode))
+				Expect(loggerMock.ErrorMSG).To(Equal("Create Group Error"))
+				Expect(loggerMock.ErrorAny).To(Equal([]interface{}{"REQUEST_ID", "123", "CODE_ERROR", "DJ-401", "ERROR_MESSAGE", "json: cannot unmarshal number into Go struct field GroupInputDTO.code of type string"}))
 			})
 		})
 	})
