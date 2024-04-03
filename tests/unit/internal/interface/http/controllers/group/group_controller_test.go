6f75b1d5e637a7de9d9d36e06078f6e43d89fa80
andreis3 <andrei.as3@hotmail.com>
2024-04-02 22:42:09 -0300

    refactor: adjust test group_controller_test

--- tests/unit/internal/interface/http/controllers/group/group_controller_test.go
+++ tests/unit/internal/interface/http/controllers/group/group_controller_test.go
@@ -94,78 +94,6 @@ var _ = Describe("INTERFACE :: HTTP :: CONTROLLERS :: GROUP :: GROUP_CONTROLLER"
 				Expect(loggerMock.ErrorAny).To(Equal([]interface{}{"REQUEST_ID", "123", "CODE_ERROR", "VBR-400", "ERROR_MESSAGE", "error test"}))
 			})
 
-			It("Should create a new group without errors", func() {
-				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
-					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
-						return group_dto.GroupOutputDTO{
-							ID:        "123",
-							GroupName: "test 1",
-							Code:      "23",
-							Status:    "active",
-							CreatedAt: "23/09/2021 10:00:00",
-							UpdatedAt: "23/09/2021 10:00:00",
-						}, nil
-					},
-				}
-				requestIDMock = &helpers_mock.RequestIDMock{
-					GenerateFunc: func() string {
-						return "123"
-					},
-				}
-				loggerMock = &logger_mock.LoggerMock{}
-
-				groupController = group_controller.NewGroupController(groupCommandMock, loggerMock, requestIDMock)
-				body := `{
-    						"group_name":"teste 1",
-							"code": "23",
-    						"status":"active"
-						  }`
-				request, err := http.NewRequest("POST", "/group", strings.NewReader(body))
-				writer := httptest.NewRecorder()
-
-				expected := helpers.TypeResponseSuccess{
-					RequestID:  "123",
-					StatusCode: http.StatusCreated,
-					Data: map[string]any{
-						"created_at": "23/09/2021 10:00:00",
-						"updated_at": "23/09/2021 10:00:00",
-						"id":         "123",
-						"group_name": "test 1",
-						"code":       "23",
-						"status":     "active",
-					},
-				}
-
-				result := helpers.TypeResponseSuccess{}
-
-				groupController.CreateGroup(writer, request)
-
-				json.Unmarshal(writer.Body.Bytes(), &result)
-
-				Expect(err).To(BeNil())
-				Expect(request).NotTo(BeNil())
-				Expect(writer.Code).To(Equal(http.StatusCreated))
-				Expect(result).To(Equal(expected))
-				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
-				Expect(result.StatusCode).To(Equal(expected.StatusCode))
-				Expect(groupCommandMock.FuncParamsInput).To(Equal([]any{group_dto.GroupInputDTO{
-					GroupName: "teste 1",
-					Code:      "23",
-					Status:    "active",
-				}}))
-				Expect(groupCommandMock.FuncParamsOutput).To(Equal([]any{
-					group_dto.GroupOutputDTO{
-						ID:        "123",
-						GroupName: "test 1",
-						Code:      "23",
-						Status:    "active",
-						CreatedAt: "23/09/2021 10:00:00",
-						UpdatedAt: "23/09/2021 10:00:00",
-					},
-					(*util.ValidationError)(nil),
-				}))
-			})
-
 			It("Should return a error when of method DecoderBodyRequest is call with poorly formatted payload", func() {
 				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
 					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
@@ -311,6 +239,78 @@ var _ = Describe("INTERFACE :: HTTP :: CONTROLLERS :: GROUP :: GROUP_CONTROLLER"
 				Expect(result.StatusCode).To(Equal(expected.StatusCode))
 				Expect(loggerMock.ErrorMSG).To(Equal("Create Group Error"))
 				Expect(loggerMock.ErrorAny).To(Equal([]interface{}{"REQUEST_ID", "123", "CODE_ERROR", "DJ-401", "ERROR_MESSAGE", "json: cannot unmarshal number into Go struct field GroupInputDTO.code of type string"}))
+			})
+
+			It("Should create a new group without errors", func() {
+				groupCommandMock = &group_command_mock.InsertGroupCommandMock{
+					ExecuteFunc: func(data group_dto.GroupInputDTO) (group_dto.GroupOutputDTO, *util.ValidationError) {
+						return group_dto.GroupOutputDTO{
+							ID:        "123",
+							GroupName: "test 1",
+							Code:      "23",
+							Status:    "active",
+							CreatedAt: "23/09/2021 10:00:00",
+							UpdatedAt: "23/09/2021 10:00:00",
+						}, nil
+					},
+				}
+				requestIDMock = &helpers_mock.RequestIDMock{
+					GenerateFunc: func() string {
+						return "123"
+					},
+				}
+				loggerMock = &logger_mock.LoggerMock{}
+
+				groupController = group_controller.NewGroupController(groupCommandMock, loggerMock, requestIDMock)
+				body := `{
+    						"group_name":"teste 1",
+							"code": "23",
+    						"status":"active"
+						  }`
+				request, err := http.NewRequest("POST", "/group", strings.NewReader(body))
+				writer := httptest.NewRecorder()
+
+				expected := helpers.TypeResponseSuccess{
+					RequestID:  "123",
+					StatusCode: http.StatusCreated,
+					Data: map[string]any{
+						"created_at": "23/09/2021 10:00:00",
+						"updated_at": "23/09/2021 10:00:00",
+						"id":         "123",
+						"group_name": "test 1",
+						"code":       "23",
+						"status":     "active",
+					},
+				}
+
+				result := helpers.TypeResponseSuccess{}
+
+				groupController.CreateGroup(writer, request)
+
+				json.Unmarshal(writer.Body.Bytes(), &result)
+
+				Expect(err).To(BeNil())
+				Expect(request).NotTo(BeNil())
+				Expect(writer.Code).To(Equal(http.StatusCreated))
+				Expect(result).To(Equal(expected))
+				Expect(result.RequestID).To(BeAssignableToTypeOf(expected.RequestID))
+				Expect(result.StatusCode).To(Equal(expected.StatusCode))
+				Expect(groupCommandMock.FuncParamsInput).To(Equal([]any{group_dto.GroupInputDTO{
+					GroupName: "teste 1",
+					Code:      "23",
+					Status:    "active",
+				}}))
+				Expect(groupCommandMock.FuncParamsOutput).To(Equal([]any{
+					group_dto.GroupOutputDTO{
+						ID:        "123",
+						GroupName: "test 1",
+						Code:      "23",
+						Status:    "active",
+						CreatedAt: "23/09/2021 10:00:00",
+						UpdatedAt: "23/09/2021 10:00:00",
+					},
+					(*util.ValidationError)(nil),
+				}))
 			})
 		})
 	})
