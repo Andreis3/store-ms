d2d7a7a9d93a676e51081a6e46a6842ad6e0c0d3
Andreis3 <andrei.as3@hotmail.com>
2024-04-08 14:17:18 -0300

    refactor: adjust router register metric and healthcheck

--- internal/infra/router/router.go
+++ internal/infra/router/router.go
@@ -1,43 +1,36 @@
 package router
 
 import (
-	healthcheck_controller "github.com/andreis3/stores-ms/internal/interface/http/controller/healthcheck"
-	metric_controller "github.com/andreis3/stores-ms/internal/interface/http/controller/metric"
+	"github.com/andreis3/stores-ms/internal/interface/http/controller/healthcheck"
+	"github.com/andreis3/stores-ms/internal/interface/http/controller/metric"
 	"net/http"
 
 	"github.com/andreis3/stores-ms/internal/infra/router/interfaces"
 	"github.com/andreis3/stores-ms/internal/interface/http/controller/group/interfaces"
-	stores "github.com/andreis3/stores-ms/internal/interface/http/controller/stores/interfaces"
+	"github.com/andreis3/stores-ms/internal/interface/http/controller/stores/interfaces"
 )
 
 type Router struct {
 	router         *http.ServeMux
 	registerRouter irouter.IRegisterRouter
-	storesRouter   stores.IStoresRouter
+	storesRouter   istores_controller.IStoresRouter
 	groupRouter    igroup_controller.IGroupRouter
-	metric         metric_controller.Router
-	healthcheck    healthcheck_controller.Router
 }
 
 func NewRouter(server *http.ServeMux,
 	registerRouter irouter.IRegisterRouter,
-	storesRouter stores.IStoresRouter,
+	storesRouter istores_controller.IStoresRouter,
 	groupRouter igroup_controller.IGroupRouter) *Router {
-	metric := metric_controller.NewMetricRouter()
-	healthcheck := healthcheck_controller.NewHealthCheckRouter()
 	return &Router{
 		router:         server,
 		registerRouter: registerRouter,
 		storesRouter:   storesRouter,
 		groupRouter:    groupRouter,
-		metric:         *metric,
-		healthcheck:    *healthcheck,
 	}
 }
-
 func (r *Router) ApiRoutes() {
 	r.registerRouter.Register(r.router, r.storesRouter.StoresRoutes())
 	r.registerRouter.Register(r.router, r.groupRouter.GroupRoutes())
-	r.registerRouter.Register(r.router, r.healthcheck.HealthCheckRoutes())
-	r.registerRouter.Register(r.router, r.metric.MetricRoutes())
+	r.registerRouter.Register(r.router, healthcheck_controller.NewHealthCheckRouter().HealthCheckRoutes())
+	r.registerRouter.Register(r.router, metric_controller.NewMetricRouter().MetricRoutes())
 }
