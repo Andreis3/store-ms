42b5102cfe4a01bf46ed649ebd2ace98213605ee
andreis3 <andrei.as3@hotmail.com>
2024-04-08 23:34:27 -0300

    test: implements test insert_group_command

---
+++ tests/unit/internal/app/command/group_command/group.command.suite_test.go
@@ -0,0 +1,22 @@
+//go:build unit
+// +build unit
+
+package group_command_test
+
+import (
+	"testing"
+
+	. "github.com/onsi/ginkgo/v2"
+	. "github.com/onsi/gomega"
+)
+
+func Test_GroupCommandSuite(t *testing.T) {
+	suiteConfig, reporterConfig := GinkgoConfiguration()
+
+	suiteConfig.SkipStrings = []string{"SKIPPED", "PENDING", "NEVER-RUN", "SKIP"}
+	reporterConfig.FullTrace = true
+	reporterConfig.Succinct = true
+
+	RegisterFailHandler(Fail)
+	RunSpecs(t, "Group Command Insert Test Suite ", suiteConfig, reporterConfig)
+}
