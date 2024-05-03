813c9ffdbbca47f9e120bec71e644e6531b65a9a
andreis3 <andrei.as3@hotmail.com>
2024-05-02 22:28:54 -0300

    feat: return error group not found in search_group_service

--- internal/domain/service/group/search_group_service.go
+++ internal/domain/service/group/search_group_service.go
@@ -1,6 +1,8 @@
 package group_service
 
 import (
+	"net/http"
+
 	"github.com/andreis3/stores-ms/internal/infra/repository/postgres/group"
 	"github.com/andreis3/stores-ms/internal/infra/repository/postgres/group/interfaces"
 	"github.com/andreis3/stores-ms/internal/infra/uow/interfaces"
@@ -22,15 +24,25 @@ func (s *SearchGroupService) SearchOneGroup(id string) (group_dto.GroupOutputDTO
 	err := s.uow.Do(func(uow iuow.IUnitOfWork) *util.ValidationError {
 		var err *util.ValidationError
 		groupRepository := uow.GetRepository(util.GROUP_REPOSITORY_KEY).(irepo_group.IGroupRepository)
-		groupModel, err = groupRepository.SelectOneGroupByID(id)
+		res, err := groupRepository.SelectOneGroupByID(id)
 		if err != nil {
 			return err
+		}
+		if res.ID == nil {
+			return &util.ValidationError{
+				Code:        "VB-0003",
+				Status:      http.StatusNotFound,
+				LogError:    []string{"group not found"},
+				ClientError: []string{"group not found"},
+				Origin:      "SearchGroupService.SearchOneGroup",
+			}
 		}
 		return nil
 	})
 	if err != nil {
 		return group_dto.GroupOutputDTO{}, err
 	}
+
 	return group_dto.GroupOutputDTO{
 		ID:        *groupModel.ID,
 		Name:      *groupModel.Name,
